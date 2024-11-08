65597161e7a6e763f3112c25296c92509a60dd0a
Lev <misterh2003@gmail.com>
2024-11-08 10:59:04 +0700

    fix linter error

--- main.py
+++ main.py
@@ -2,36 +2,46 @@ class SmartHouse:
     def __init__(self):
         self.vendor = ''
 
+
 class SmartLamp(SmartHouse):
-    def __init__(self, power = 0, plug = 'не указано', vendor = 'NoName'):
+    def __init__(self, power=0, plug='не указано', vendor='NoName'):
         self.power = power
         self.plug = plug
         self.vendor = vendor
+
     def __str__(self):
-        return 'Производитель: ' + self.vendor + ', Разьём: ' + str(self.plug) + ', Мощность: ' + str(self.power)
+        return f'Производитель: {self.vendor}, Разьём: {str(self.plug)}, Мощность: {str(self.power)}'
+
 
 class SmartPlug(SmartHouse):
-    def __init__(self, maxPower = 0, vendor = 'NoName'):
-        self.maxPower = maxPower
+    def __init__(self, max_power=0, vendor='NoName'):
+        self.max_power = max_power
         self.vendor = vendor
+
     def __str__(self):
-        return 'Производитель: ' + self.vendor + ', Максимальная мощность: ' + str(self.maxPower)
+        return f'Производитель: {self.vendor}, Максимальная мощность: {str(self.max_power)}'
+
 
 class Thermostat(SmartHouse):
-    def __init__(self, minTemp = 0, maxTemp = 0, vendor = 'NoName'):
-        self.minTemp = minTemp
-        self.maxTemp = maxTemp
+    def __init__(self, min_temp=0, max_temp=0, vendor='NoName'):
+        self.min_temp = min_temp
+        self.max_temp = max_temp
         self.vendor = vendor
-    def __str__(self):
-        return 'Производитель: ' + self.vendor + ', Мин.Температура: ' + str(self.minTemp) + ', Макс.Температура: ' + str(self.maxTemp)
 
-def removeDevice(vendor_name = ''):
+    def __str__(self):
+        return f'Производитель: {self.vendor}, Мин.Температура: {str(self.min_temp)}, Макс.Температура: {str(self.max_temp)}'
+
+
+def remove_device(vendor_name=''):
     global lampContainer, plugContainer, thermostatContainer
     lampContainer = [lamp for lamp in lampContainer if lamp.vendor != vendor_name]
     plugContainer = [plug for plug in plugContainer if plug.vendor != vendor_name]
     thermostatContainer = [thermostat for thermostat in thermostatContainer if thermostat.vendor != vendor_name]
+    print('\n----------REM----------')
+    print(f'Удалили устройства производителя {vendor_name}\n')
 
-def printLamp(index = -1):
+
+def print_lamp(index=-1):
     print('Умные лампы:')
     if index != -1:
         print('\t' + lampContainer[index].print())
@@ -39,7 +49,8 @@ def printLamp(index = -1):
         for i in lampContainer:
             print('\t' + str(i))
 
-def printPlug(index = -1):
+
+def print_plug(index=-1):
     print('Умные розетки:')
     if index != -1:
         print('\t' + plugContainer[index].print())
@@ -47,7 +58,8 @@ def printPlug(index = -1):
         for i in plugContainer:
             print('\t' + str(i))
 
-def printThermostat(index = -1):
+
+def print_thermostat(index=-1):
     print('Термостат:')
     if index != -1:
         print('\t' + thermostatContainer[index].print())
@@ -55,14 +67,16 @@ def printThermostat(index = -1):
         for i in thermostatContainer:
             print('\t' + str(i))
 
-def printSmartAllDevice():
+
+def print_smart_all_device():
     print('\n----------PRINT----------')
     if lampContainer:
-        printLamp()
+        print_lamp()
     if plugContainer:
-        printPlug()
+        print_plug()
     if thermostatContainer:
-        printThermostat()
+        print_thermostat()
+
 
 lampContainer = []
 plugContainer = []
@@ -73,17 +87,19 @@ lampContainer.append(SmartLamp(12, 'e14'))
 plugContainer.append(SmartPlug(1000, 'Xiaomi'))
 thermostatContainer.append(Thermostat(5, 100))
 
-printSmartAllDevice()
-removeDevice('NoName')
-printSmartAllDevice()
+print_smart_all_device()
+remove_device('NoName')
+print_smart_all_device()
 
 print('\n\n\n-------- Work with File --------')
 
-input_txt = open('input-oppo-2.txt', 'r')
-for i in input_txt:
+with open('input-oppo-2.txt', 'r') as input_txt:
+    contents = input_txt.read()
+
+for i in contents.split('\n'):
     command = i.split()
     if command[0] == 'PRINT':
-        printSmartAllDevice()
+        print_smart_all_device()
     elif command[0] == 'ADD':
         if command[1] == 'Лампочка':
             lampContainer.append(SmartLamp(*command[2:]))
@@ -94,6 +110,6 @@ for i in input_txt:
         else:
             print('Устройтсво не поддерживается')
     elif command[0] == 'REM':
-        removeDevice(*command[1:])
+        remove_device(*command[1:])
     else:
         print('Команда не распознана')
